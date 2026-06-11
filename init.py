20b3a553a1460f004926f763979d2d7e47b01ab7
parakosm <20062@rosmini.school.nz>
2026-06-11 12:13:27 +1200

    Added Signin-signup

--- init.py
+++ init.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 def create_app():
     app = Flask(__name__)
 
     @app.route('/')
-    def hello():
-        return "Hello, World!"
+    def index():
+        # Renders the template located at templates/signinsignup.html.jinja
+        return render_template('signinsignup.html.jinja')
 
     return app
 
-create_app()
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
